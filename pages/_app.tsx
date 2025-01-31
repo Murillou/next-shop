432cf50856983d6e1c179700b941a978e275b209
Murillou <Murillovinicius13@hotmail.com>
2025-01-31 01:01:45 -0300

    style estilização dos produtos concluída

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import { globalStyles } from '../src/styles/global';
 import type { AppProps } from 'next/app';
-
-globalStyles();
-
 import logoImg from '../src/assets/logo.svg';
 import { Container, Header } from '../src/styles/pages/app';
+import Image from 'next/image';
 
+globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
         {' '}
-        <img src={logoImg.src} alt="" />{' '}
+        <Image src={logoImg} alt="" />{' '}
       </Header>
 
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
+import { HomeContainer, Product } from '../src/styles/pages/home';
 import { styled } from '../src/styles';
+import Image from 'next/image';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  span: {
-    fontWeight: 'bold',
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-});
+import camiseta1 from '../src/assets/shirts/1.png';
+import camiseta2 from '../src/assets/shirts/3.png';
+import camiseta3 from '../src/assets/shirts/3.png';
 
 export default function Home() {
   return (
-    <Button>
-      <span>Strong</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt={''} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt={''} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }

9e3d004bc8fe39a92cddd4c8c1b910303d049791
Murillou <Murillovinicius13@hotmail.com>
2025-01-30 22:02:00 -0300

    feat inicializando projeto

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from '../styles';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,7 @@
+export default function Success() {
+  return (
+    <h1>
+      <div> Success</div>
+    </h1>
+  );
+}

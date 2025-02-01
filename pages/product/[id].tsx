4254e30d0d1657918d09a0f1b8c4d233a4e51065
Murillou <Murillovinicius13@hotmail.com>
2025-02-01 10:22:42 -0300

    style estilizando a pagina de produto

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,7 +1,27 @@
+import {
+  ProductDetails,
+  ImageContainer,
+  ProductContainer,
+} from '@/src/styles/pages/product';
+
 export default function Product() {
   return (
-    <div>
-      <h1>Product</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 79.90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste possimus
+          iusto aperiam nostrum reiciendis nam fuga. Saepe nesciunt adipisci rem
+          dignissimos molestias neque modi, necessitatibus et cumque doloremque
+          fugit repellendus.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }

59be58daa59af7fbfe323bd11428079aa37fe96c
Murillou <Murillovinicius13@hotmail.com>
2025-02-02 16:36:58 -0300

    feat estilização da página de success

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,7 +1,21 @@
+import {
+  ImageSuccessContainer,
+  SuccessContainer,
+} from '@/src/styles/pages/success';
+import Link from 'next/link';
+
 export default function Success() {
   return (
-    <h1>
-      <div> Success</div>
-    </h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageSuccessContainer></ImageSuccessContainer>
+
+      <p>
+        Uhuul <strong>Murillo Vinícius</strong>, sua{' '}
+        <strong>Camise Ignite</strong> já está a caminho da sua casa.
+      </p>
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
   );
 }

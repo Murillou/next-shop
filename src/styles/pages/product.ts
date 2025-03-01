1939bd5dced7232684613f2519a692e358ee0f58
Murillou <murillovinicius13@hotmail.com>
2025-03-01 15:04:50 -0300

    style: padronizando sizings responsivos da página product

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -10,10 +10,13 @@ export const ProductContainer = styled('main', {
   maxWidth: 500,
   margin: '0 auto',
 
-  '@media (min-width: 1024px)': {
+  '@media (min-width: 768px)': {
     gridTemplateColumns: '1fr 1fr',
-    gap: '4.5rem',
+    gap: '2rem',
     maxWidth: 1180,
+  },
+  '@media (min-width: 1280px)': {
+    gap: '4.5rem',
   },
 });
 
@@ -39,24 +42,25 @@ export const ProductDetails = styled('div', {
   flexDirection: 'column',
   maxWidth: '100%',
 
-  h1: { fontSize: '$2xl', color: '$gray300' },
+  h1: { fontSize: '20px', color: '$gray300' },
 
   span: {
     marginTop: '1rem',
     display: 'block',
-    fontSize: '$2xl',
+    fontSize: '20px',
     color: '$green300',
+    fontWeight: 'bold',
   },
 
   p: {
-    marginTop: '2.5rem',
-    fontSize: '$md',
+    marginTop: '1rem',
+    fontSize: '16px',
     lineHeight: 1.6,
     color: '$gray300',
   },
 
   button: {
-    marginTop: 'auto',
+    margin: '1rem 0',
     backgroundColor: '$green500',
     border: 0,
     borderRadius: 8,
@@ -64,7 +68,7 @@ export const ProductDetails = styled('div', {
     cursor: 'pointer',
     padding: '1.25rem',
     fontWeight: 'bold',
-    fontSize: '$md',
+    fontSize: '16px',
 
     '&:disabled': {
       cursor: 'not-allowed',
@@ -73,6 +77,19 @@ export const ProductDetails = styled('div', {
 
     '&:not(:disabled):hover': {
       backgroundColor: '$green300',
+    },
+  },
+
+  '@media (min-width: 768px)': {
+    h1: { fontSize: '$2xl' },
+
+    span: {
+      fontSize: '$2xl',
+    },
+
+    p: {
+      fontSize: '$md',
+      marginTop: '2.5rem',
     },
   },
 });
